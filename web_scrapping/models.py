7e25d36c6ad8a833533e316832bf5af1334a1862
ayusHKumar476 <mfsi.ayush@gmail.com>
2024-01-15 18:51:20 +0530

    basic app setup with migrations included

---
+++ web_scrapping/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	first_name = models.TextField(max_length=30)
+	last_name = models.TextField(max_length=30)
+	is_superuser = models.BooleanField(default=False)
+	otp = models.CharField(max_length=4, null=True, blank=True)
+
+	def __str__(self):
+		return self.user
+
+
+class Store(models.Model):
+	store_name = models.CharField(max_length=50)
+	store_address = models.CharField(max_length=50)
+	phone_number = models.CharField(max_length=50)
+	zip_code = models.CharField(max_length=50)
+	latitude = models.CharField(max_length=50)
+	longitude = models.CharField(max_length=50)
+	city_name = models.CharField(max_length=50)
+	state = models.CharField(max_length=25)
+
+	def __str__(self):
+		return self.store_name
+
+
+class ScrapedData(models.Model):
+	store = models.ForeignKey(Store, on_delete=models.CASCADE)
+	data = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.store.store_name} - {self.timestamp}"
---
+++ web_scrapping/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from web_scrapping.models import Store
+from rest_framework.generics import ListCreateAPIView
+
+# Create your views here.
